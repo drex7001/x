a93f973130924842f6841c0af78e7c065df8d346
Ayodhya Ratnayake <ayodhya@tes.lk>
2025-01-07 17:29:24 +0530

    quiz

---
+++ quiz/text-compression/main.py
@@ -0,0 +1,22 @@
+def compressString(s):
+    n = len(s)
+    i = 0
+    res = ""
+    while i < n:
+        count = 1
+        while i + 1 < n and s[i] == s[i + 1]:
+            i += 1
+            count += 1
+        res += s[i] + str(count)
+        i += 1
+    return res
+
+# Get input from the user
+
+print(compressString("AABBBCCCC"))
+print(compressString("AaBbCc"))
+print(compressString(""))
+print(compressString("ABCDEFG"))
+print(compressString("AAABBA"))
+print(compressString("A"))
+print(compressString("AAAAAAAAAA"))
